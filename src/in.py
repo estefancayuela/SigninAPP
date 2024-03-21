3797e9f78fa5d7352a2f4ae1973e2d6eb1e022df
Estefan Cayuela <cayuelaestefan@gmail.com>
2024-03-21 10:43:14 +0100

    SignIn working
    SignIn working with custom users and hours, half or o'clock.

--- src/in.py
+++ src/in.py
@@ -1,22 +1,15 @@
 import random
 import json
 import csv
-
-
-file_json = open('data/data.json')
-data = json.loads(file_json.read())
-
-#Get the name from the 1st user example: user = data["users"][1]["name"]
-
-
+from datetime import date
+import holidays
 
 #
 #USERS LOGIN
 #
-
 class Employee:
 
-    def random_oclock(hour:int ):
+    def random_oclock(hour):
         hour_before = hour-1
         current_hour = random.choice([hour, hour_before])
         if current_hour == hour:
@@ -28,15 +21,36 @@ class Employee:
 
 
 
-    def random_half(hour:int):
+    def random_half(hour):
         minutes = random.randint(28,35)
         return "{}:{}".format(hour, minutes)
 
 
+#
+#HOLDAY CHECK
+#To chenge the location/file we have to modify the open() function
+#Get the name from the 1st user example: user = data["users"][0]["name"]
+#
 
-with open("docs/march.csv", 'a', newline='') as file:
-    csvwriter = csv.writer(file)
-    for user in data["users"]:
-        csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_in"]), Employee.random_half(user["hour_out"])])
-
-
+with open("data/data.json") as file_json:
+    data = json.loads(file_json.read())
+    year = data["year"][0]["current"]
+    if not date.today() in sorted(holidays.ES(subdiv='CT', years=year).items()):
+        with open("docs/march.csv", 'a', newline='') as file:
+            csvwriter = csv.writer(file)
+            #SignIn log
+            for user in data["users"]:
+                if user["min_in"] == 0:
+                    csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_in"]), date.today(), "CheckIn"])
+                elif user["min_in"] == 30:
+                    csvwriter.writerow([user["name"],Employee.random_half(user["hour_in"]), date.today(), "CheckIn"])
+                else:
+                    print("Incorrect value range: the user {} only can SignIn at {}:0, or a {}:30".format(user["name"], user["hour_in"], user["hour_in"]))
+            #SignOut log
+            for user in data["users"]:
+                if user["min_out"] == 0:
+                    csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_out"]), date.today(), "CheckOut"])
+                elif user["min_out"] == 30:
+                    csvwriter.writerow([user["name"],Employee.random_half(user["hour_out"]), date.today(), "CheckOut"])
+                else:
+                    print("Incorrect value range: the user {} only can SignOut at {}:0, or a {}:30".format(user["name"], user["hour_out"], user["hour_out"]))
