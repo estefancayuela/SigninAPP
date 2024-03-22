31d6f4d2f255ed693de6e47d288cf6ab81605167
Estefan Cayuela <cayuelaestefan@gmail.com>
2024-03-22 17:44:33 +0100

    Functions about Sign, done
    We can do 3 functions: Sing all the year still today, Sign for months or Sign Today

--- src/in.py
+++ src/in.py
@@ -4,6 +4,11 @@ import csv
 from datetime import date
 import holidays
 
+
+#Dict with holidays from your country
+cat_holidays = holidays.ES(subdiv="CT")
+
+
 #
 #USERS LOGIN
 #
@@ -31,10 +36,12 @@ class Employee:
         minutes = random.randint(28,35)
         return "{}:{}".format(hour, minutes)
 
-"""
+
+
 class Months():
     def __init__(self):
         self.days = 28
+
         
     def month_days(self):
         for i in range(self.days):
@@ -44,6 +51,7 @@ class Months():
 class Month_30(Months):
     def __init__(self):
         self.days = 30
+
         
         
 
@@ -52,48 +60,12 @@ class Month_31(Months):
         self.days = 31
 
 
+
 months_with_30 = ["04","06","09","11"]
-months_with_31 = ["01","03","05","07","08",10,12]
+months_with_31 = ["01","03","05","07","08","10","12"]
 months_with_28 = ["02"]
-"""
-
-current_day = date.today().weekday()
-current_month = date.today().strftime("%m")
-current_year = date.today().strftime("%Y")
-def dia_avui():
-    if current_day == 0:
-        print("Dilluns")
-    elif current_day == 1:
-        print("Dimarts")
-    elif current_day == 2:
-        print("Dimecres")
-    elif current_day == 3:
-        print("Dijous")
-    elif current_day == 4:
-        print("Divendres")
-    elif current_day == 5:
-        print("Dissabte")
-    elif current_day == 6:
-        print("Diumenge")
 
 
-"""
-if current_mes in months_with_30:
-    current_mes = Month_30()
-    print(current_mes.days)
-
-elif current_mes in months_with_31:
-    current_mes = Month_31()
-    print(current_mes.days)
-
-elif current_mes in months_with_28:
-    current_mes = Months()
-    print(current_mes.days)
-
-else:
-    print("Error: couldn't get the date")
-
-"""
 
 #
 #SIGNIN TODAY
@@ -101,35 +73,112 @@ else:
 #Get the name from the 1st user example: user = data["users"][0]["name"]
 #
 def today_check():
+    m_date = date.today().strftime("%m")
+    m_date_int = date_to_int(m_date)
+    current_year_int = int(date.today().strftime("%Y"))
+    lets_check(1, current_year_int, m_date_int, m_date)
+
+
+#
+#SignIn and Out fully month
+#
+
+def month_check(m_date):
+    m_date_int = date_to_int(m_date)
+    current_year_int = int(date.today().strftime("%Y"))
+
+    #First we check if the month selected is the actually month, if it is, we will SignIn still today
+    if date.today().strftime("%m") == m_date:
+        month_day_int = int(date.today().strftime("%d"))
+        lets_check(month_day_int, current_year_int, m_date_int, m_date)
+
+    #If the month selected is pasted, we will do the SignIn and Out of this month.
+    else:
+        days = assign_days(m_date)
+        lets_check(days, current_year_int, m_date_int, m_date)
+
+
+
+def lets_check(days:int, current_year_int:int, m_date_int:int, m_date:str):
+
+    #Reset de month day at 1st
+    m_day = 1
+
     with open("data/data.json") as file_json:
         data = json.loads(file_json.read())
-        year = data["year"][0]["current"]
-        if not date.today() in sorted(holidays.ES(subdiv='CT', years=year).items()):
-            with open("docs/{}_{}.csv".format(current_year, current_month), 'a', newline='') as file:
-                csvwriter = csv.writer(file)
-            
-                #SignIn log
-                for user in data["users"]:
-                    if user["min_in"] == 0:
-                        csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_in"]), date.today(), "CheckIn"])
-                    
-                    elif user["min_in"] == 30:
-                        csvwriter.writerow([user["name"],Employee.random_half(user["hour_in"]), date.today(), "CheckIn"])
-                    
-                    else:
-                        print("Incorrect value range: the user {} only can SignIn at {}:0, or a {}:30".format(user["name"], user["hour_in"], user["hour_in"]))
+        #We will check day for day to do the Sign
+        for i in range(days):
+            current_date = date(year=current_year_int, month=m_date_int, day=m_day)
+            if not current_date in cat_holidays and current_date.weekday() != 6:
+                with open("docs/{}_{}.csv".format(current_year_int, m_date), 'a', newline='') as file:
+                    csvwriter = csv.writer(file)
                 
-                #SignOut log
-                for user in data["users"]:
-                    if user["min_out"] == 0:
-                        csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_out"]), date.today(), "CheckOut"])
-                    
-                    elif user["min_out"] == 30:
-                        csvwriter.writerow([user["name"],Employee.random_half(user["hour_out"]), date.today(), "CheckOut"])
-                    
-                    else:
-                        print("Incorrect value range: the user {} only can SignOut at {}:0, or a {}:30".format(user["name"], user["hour_out"], user["hour_out"]))   
+                    #SignIn log
+                    for user in data["users"]:
+                        if user["min_in"] == 0:
+                            csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_in"]), current_date, "CheckIn"])
+                        
+                        elif user["min_in"] == 30:
+                            csvwriter.writerow([user["name"],Employee.random_half(user["hour_in"]), current_date, "CheckIn"])
+                        
+                        else:
+                            print("Incorrect value range: the user {} only can SignIn at {}:0, or at {}:30".format(user["name"], user["hour_in"], user["hour_in"]))
+                        
+                    #SignOut log
+                    for user in data["users"]:
+                        if user["min_out"] == 0:
+                            csvwriter.writerow([user["name"], Employee.random_oclock(user["hour_out"]), current_date, "CheckOut"])
+                            
+                        elif user["min_out"] == 30:
+                            csvwriter.writerow([user["name"],Employee.random_half(user["hour_out"]), current_date, "CheckOut"])
+                            
+                        else:
+                            print("Incorrect value range: the user {} only can SignOut at {}:0, or at {}:30".format(user["name"], user["hour_out"], user["hour_out"]))   
+            m_day = m_day + 1
 
 
 
-today_check()
+
+#Doing all Singns still today (at the same year)
+def check_still_today():
+    m_date = date.today().strftime("%m")
+    m_date_int = date_to_int(m_date)
+
+    for i in range(m_date_int-1):
+        m_date_int = m_date_int -1
+        if m_date_int < 9:
+            month_check("0{}".format(str(m_date_int))) #We need to give it as string to operate with it
+        else:
+            month_check(str(m_date_int))
+    
+    month_check(m_date)
+
+
+
+def assign_days(m_date:object):
+    #Assigning the selected month the days it will have.
+    if m_date in months_with_31:
+        m_date = Month_31()
+    elif m_date in months_with_30:
+        m_date = Month_30()
+    elif m_date in months_with_28:
+        m_date = Months()
+    
+    return(m_date.days)
+
+
+
+#Convert m_date:str into m_date int, to operate with it
+def date_to_int(m_date:str):
+    if m_date == "01" or m_date == "02" or m_date == "03" or m_date == "04" or m_date == "05" or m_date == "06" or m_date == "07" or m_date == "08" or m_date == "09":
+        m_date_int = int(m_date[-1])
+    else:
+        m_date_int = int(m_date)
+    
+    return m_date_int
+
+
+
+#check_still_today()
+#today_check()
+#month_check("01")
